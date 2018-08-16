20cbdd20e872c125cc141557e48a09e55ca066fb
jarretbryan <jarret.bryan@gmail.com>
2018-08-16 12:00:22 -0400

    build out user controller; instantiate mode; create first migration;

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,36 @@
+class Api::V1::UsersController < ApplicationController
+
+    def index
+        @users = User.all
+        render json: @users
+    end
+
+    def show
+        render json: @user
+    end
+
+
+    def create
+        @user = User.find_or_create_by(user_params)
+        if @user.save
+            render json: @user, status: :accepted
+        else
+            render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
+        end
+    end
+
+    def destroy
+        @user.delete 
+    end
+
+
+    private
+
+    def get_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.permit(:name)
+    end
+end
